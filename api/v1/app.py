6d1c40298898eb73954d1b5e902ca90be53c0666
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-09-16 15:46:34 -0500

    adding task 6

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,6 +4,7 @@ from flask import Flask
 from models import storage
 from api.v1.views import app_views
 from os import getenv
+from flask import jsonify
 
 
 app = Flask(__name__)
@@ -13,6 +14,11 @@ app.register_blueprint(app_views)
 @app.teardown_appcontext
 def teardown(exception):
     storage.close()
+
+
+@app.errorhandler(404)
+def error_404(e):
+    return jsonify(error="Not found")
 
 
 if __name__ == "__main__":
