3f70744ec0cad037a843b68f5ca88a3a3b29d3e9
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-09-16 15:49:06 -0500

    adding task 6

--- api/v1/app.py
+++ api/v1/app.py
@@ -18,7 +18,7 @@ def teardown(exception):
 
 @app.errorhandler(404)
 def error_404(e):
-    return jsonify(error="Not found")
+    return jsonify(error="Not found"), 404
 
 
 if __name__ == "__main__":
