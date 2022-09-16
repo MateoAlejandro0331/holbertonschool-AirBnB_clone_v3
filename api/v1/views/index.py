4bb513499101a0cdb21ea02a6f1b5664562922ab
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-09-16 12:24:45 -0500

    adding task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Flask instance"""
+from flask import jsonify
 from api.v1.views import app_views
-
 
 
 @app_views.route("/status", strict_slashes=False)
 def status():
-    return "Ok"
+    return jsonify(status="ok")
